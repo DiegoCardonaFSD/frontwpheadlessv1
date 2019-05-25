325d954014ee1679656de919193e45fe6d1b9e81
Diego Cardona <diego@thedeveloper.co>
2019-05-25 08:34:37 -0500

    installing and configuring axios to the project to make ajax calls

--- src/store.js
+++ src/store.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
-Vue.use(Vuex)
+Vue.use(Vuex, axios)
 
 export default new Vuex.Store({
   state: {
-
+    interviews: [
+      {interviewer: {id:''}, candidate: {id: ''}, date: '', meetinglink: '',rate: '', status: ''},
+    ]
   },
   mutations: {
 
